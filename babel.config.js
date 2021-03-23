726a077652030543cbfeff2f008f19cc45663865
Pham Sy Hung <hungps.work@gmail.com>
2021-03-23 20:54:16 +0700

    setup babel module resolver

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,15 @@
 module.exports = {
+  plugins: [
+    [
+      "module-resolver",
+      {
+        root: ["./"],
+        extensions: [".ts", ".tsx"],
+        alias: {
+          "^\\/(.+)": "./src/\\1",
+        },
+      },
+    ],
+  ],
   presets: ["module:metro-react-native-babel-preset"],
 }
